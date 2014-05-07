9209c56fdb2d30cc91eca50fd4aaedce714c44fe
David Ammeraal <david.ammeraal@gmail.com>
2014-05-07 17:11:55 +0200

    Added a first basic implementation of the item page in item.html.

--- js/item.js
+++ js/item.js
@@ -1,1 +1,50 @@
+$(document).ready(function(){
+    /*
+    var wWidth, wHeight, mobileElements = $('.mobile'),
+        desktopElements = $('.desktop');
 
+    function getWindowSize(){
+        vWidth = $(window).width();
+        vHeight = $(window).height();
+    };
+
+    function hideMobileElements(){
+        console.log("hideMobileElements()");
+        mobileElements.hide();
+    };
+
+    function hideDesktopElements(){
+        console.log("hideDesktopElements()");
+        desktopElements.hide();
+    };
+
+    function showMobileElements(){
+        console.log("showMobileElements()");
+        mobileElements.show();
+    };
+
+    function showDesktopElements(){
+        console.log("showDesktopElements();")
+        desktopElements.show();
+    };
+
+    function showElementsForWindowSize(){
+        if(vWidth < 800){
+            hideDesktopElements();
+            showMobileElements();
+        }else{
+            hideMobileElements();
+            showDesktopElements();
+        }
+    };
+
+    function onResize(){
+        getWindowSize();
+        showElementsForWindowSize();
+    }
+
+    $(window).on('resize', onResize);
+    onResize();
+
+    */
+});
