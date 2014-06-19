74cc4b0ceec9dfe72ced023968a80de805a50e95
Akbar Khrisna <a.khrisna@live.com>
2014-06-19 12:09:17 +0700

    Add temporary plugin to slide the menu
    NOTE: To be remove, once David adapts the jquery.slide-panel.js to
    mimic the behavior of this plugin (pushes the content to the side)

--- js/plugins.js
+++ js/plugins.js
@@ -22,3 +22,79 @@
 }());
 
 // Place any jQuery/helper plugins in here.
+
+/*
+    slidePanelJS
+*/
+(function($) {
+	$.fn.slidePanelJS = function(customSettings) {
+		var settings = $.extend({
+            openButton: 	'#openButton' ,
+            pageSection:	'#pageSection',
+            navbarSection:	'#navbarSection',
+            speed:		   	200                
+        }, customSettings || {});
+        
+        // vars 
+        settings.openButton = $(settings.openButton);
+        settings.body = $('body');
+        settings.html = $('html');
+        settings.pageSection = $(settings.pageSection);
+        settings.navbarSection = $(settings.navbarSection);
+        var obj = this;
+        
+        // events
+        settings.openButton.click(function(event){
+            event.preventDefault();
+        });
+        
+        // init
+        var init = function() {
+        	settings.openButton.click(function(){
+	            if (obj.hasClass('open')) {
+	                slideIn();
+	            } else {
+		            slideOut();
+	            }
+            });
+        };
+        
+        // functions
+        var slideOut = function() {
+        	
+        	// set style
+        	obj.addClass("open");
+        	settings.openButton.addClass("active");
+        	settings.pageSection.addClass('slided');
+        	
+        	// animate
+        	obj.animate({left:0}, settings.speed);
+        	settings.pageSection.animate({left:obj.width()}, settings.speed);
+        	settings.navbarSection.css({width:'100%'}).animate({left:obj.width()}, settings.speed);
+        	
+        	// hide scrolling
+	        settings.body.css({'overflow-x':'hidden'});
+	        settings.html.css({'overflow-x':'hidden'});
+        };
+        
+        var slideIn = function() {
+        
+        	// set style
+        	obj.removeClass("open");
+        	settings.openButton.removeClass("active");
+        	
+        	// animate
+        	obj.animate({left:'-'+obj.width()}, settings.speed);
+        	settings.pageSection.animate({left:0}, settings.speed);
+        	settings.navbarSection.animate({left:0}, settings.speed, function(){ settings.pageSection.removeClass('slided'); });
+        	
+        	// reset scrolling
+	        settings.body.css({'overflow-x':'auto'});
+	        settings.html.css({'overflow-x':'auto'});
+        };
+        
+        // init
+        init();
+        
+	};
+})(jQuery);
