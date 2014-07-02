b17d00fe1856607092d43050d6cc042c1f7d1963
Akbar Khrisna <a.khrisna@live.com>
2014-07-02 11:00:05 +0700

    General UI Development
    First version of homepage HTML/CSS

--- js/home.js
+++ js/home.js
@@ -5,11 +5,37 @@ $(document).ready(function () {
 
     EUScreenXL.HomePage = function () {
         EUScreenXL.Page.apply(this, arguments);
+        this.$navElement = jQuery('#navpanel');
+        this.$navbarElement = jQuery('.navbar-header');
+        this.$formElement = jQuery('#headerform');
+
+         // nav panel
+        this.$navElement.slidePanelJS({
+            openButton: '#menubutton',
+            pageSection:'#page',
+            navbarSection:'#navbar',
+            speed:200
+        });
     };
 
     EUScreenXL.HomePage.prototype = Object.create(EUScreenXL.Page.prototype);
+    EUScreenXL.HomePage.prototype.searchButton = jQuery("#searchbutton");
+    EUScreenXL.HomePage.prototype.menuButton = jQuery("#menubutton");
     EUScreenXL.HomePage.prototype.name = "home";
     EUScreenXL.HomePage.prototype.events = {
+
+        'click #searchbutton': function(event) {
+            if(this.searchButton.hasClass("active")) {
+                this.searchButton.removeClass("active"); // toggle style
+                this.$navbarElement.removeClass('searchOpened');
+                this.menuButton.show();
+            } else {
+                this.searchButton.addClass("active"); // toggle style
+                this.$navbarElement.addClass('searchOpened');
+                this.$formElement.find('input[type="text"]').focus();
+                this.menuButton.hide();
+            }
+        }
     }
 
     new EUScreenXL.HomePage();
