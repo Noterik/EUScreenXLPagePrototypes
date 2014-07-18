dafdf362b4e9a427c2076c317a286c0f8ed4d2a7
Akbar Khrisna <a.khrisna@live.com>
2014-07-18 17:42:31 +0700

    General development of about page

--- js/about.js
+++ js/about.js
@@ -6,6 +6,8 @@ $(document).ready(function () {
     EUScreenXL.AboutPage = function () {
         EUScreenXL.Page.apply(this, arguments);
         this.$navElement = jQuery('#navpanel');
+        this.$navbarElement = jQuery('.navbar-header');
+        this.$formElement = jQuery('#headerform');
         
         // nav panel
         this.$navElement.slidePanelJS({
@@ -17,8 +19,22 @@ $(document).ready(function () {
     };
 
     EUScreenXL.AboutPage.prototype = Object.create(EUScreenXL.Page.prototype);
+    EUScreenXL.AboutPage.prototype.searchButton = jQuery("#searchbutton");
+    EUScreenXL.AboutPage.prototype.menuButton = jQuery("#menubutton");
     EUScreenXL.AboutPage.prototype.name = "about";
     EUScreenXL.AboutPage.prototype.events = {
+    	'click #searchbutton': function(event) {
+            if(this.searchButton.hasClass("active")) {
+                this.searchButton.removeClass("active"); // toggle style
+                this.$navbarElement.removeClass('searchOpened');
+                this.menuButton.show();
+            } else {
+                this.searchButton.addClass("active"); // toggle style
+                this.$navbarElement.addClass('searchOpened');
+                this.$formElement.find('input[type="text"]').focus();
+                this.menuButton.hide();
+            }
+        }
     }
 
     new EUScreenXL.AboutPage();
