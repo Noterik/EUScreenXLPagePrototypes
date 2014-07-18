adbbf2f8138a86d7fb037fecd938e65a7c37f3e7
Akbar Khrisna <a.khrisna@live.com>
2014-07-18 17:27:06 +0700

    Logo Fix

---
+++ js/min/search-results-ck.js
@@ -0,0 +1,59 @@
+$(document).ready(function () {
+	
+    if (!EUScreenXL) {
+        throw "EUScreenXL required libraries not there!";
+    }
+
+    EUScreenXL.SearchResultsPage = function () {
+        EUScreenXL.Page.apply(this, arguments);
+        this.$navElement = jQuery('#navpanel');
+        this.$sideBarElements = jQuery('#sidebar');
+        this.$searchInputElement = jQuery('#searchkeyword');
+        
+        // nav panel
+        this.$navElement.slidePanelJS({
+	        openButton: '#menubutton',
+            pageSection:'#page',
+            navbarSection:'#navbar',
+            speed:200
+        });
+    };
+
+    EUScreenXL.SearchResultsPage.prototype = Object.create(EUScreenXL.Page.prototype);
+    EUScreenXL.SearchResultsPage.prototype.name = "search-results";
+    EUScreenXL.SearchResultsPage.prototype.extraFilters = jQuery(".extra-option");
+    EUScreenXL.SearchResultsPage.prototype.showExtraFiltersButton = jQuery("#show-extra-filters");
+    EUScreenXL.SearchResultsPage.prototype.hideExtraFiltersButton = jQuery("#hide-extra-filters");
+    EUScreenXL.SearchResultsPage.prototype.optionButton = jQuery("#optionbutton");
+    EUScreenXL.SearchResultsPage.prototype.searchButton = jQuery("#searchbutton");
+    EUScreenXL.SearchResultsPage.prototype.events = {
+        "click #show-extra-filters": function (event) {
+            this.showExtraFiltersButton.hide();
+            this.hideExtraFiltersButton.show();
+        },
+        "click #hide-extra-filters": function (event) {
+            this.hideExtraFiltersButton.hide();
+            this.showExtraFiltersButton.show();
+        },
+        "show.bs.collapse .filtercontent": function(event) {
+        	$("#"+event.currentTarget.id).parent().find("i").removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-up");
+        },
+        "hide.bs.collapse .filtercontent": function(event) {
+	    	$("#"+event.currentTarget.id).parent().find("i").removeClass("glyphicon-chevron-up").addClass("glyphicon-chevron-down");
+        },
+        "click #optionbutton": function(event) {
+        	if(this.optionButton.hasClass("active")) {
+	        	this.optionButton.removeClass("active"); // toggle style
+	        	this.$sideBarElements.removeClass("optionOpened"); // will hide the filters and media selector
+        	} else {
+	        	this.optionButton.addClass("active"); // toggle style
+	        	this.$sideBarElements.addClass("optionOpened"); // will show the filters and media selector
+        	}
+        },
+        "click #searchbutton": function(event) {
+	        this.$searchInputElement.focus(); // focus on the search field, instead of opening the searchbox on top
+        }
+    }
+
+    new EUScreenXL.SearchResultsPage();
+});
