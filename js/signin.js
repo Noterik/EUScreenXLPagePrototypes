82f57969966a03430657ad58dddce02d377aaba8
Akbar Khrisna <a.khrisna@live.com>
2014-10-16 17:39:07 +0700

    General Development

---
+++ js/signin.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+    if (!EUScreenXL) {
+        throw "EUScreenXL required libraries not there!";
+    }
+
+    EUScreenXL.SignInPage = function () {
+        EUScreenXL.Page.apply(this, arguments);        
+        this.$navElement = jQuery('#navpanel');
+        this.$navbarElement = jQuery('.navbar-header');
+        this.$formElement = jQuery('#headerform');
+        this.$overlayContents = jQuery('.overlaycontent');
+        this.$overlayButtons = jQuery('button[data-overlay]');
+        var obj = this;
+
+        // nav panel
+        this.$navElement.slidePanelJS({
+            openButton: '#menubutton',
+            pageSection:'#page',
+            navbarSection:'#navbar',
+            speed:200
+        });
+    };
+
+    EUScreenXL.SignInPage.prototype = Object.create(EUScreenXL.Page.prototype);
+    EUScreenXL.SignInPage.prototype.searchButton = jQuery("#searchbutton");
+    EUScreenXL.SignInPage.prototype.menuButton = jQuery("#menubutton");
+    EUScreenXL.SignInPage.prototype.name = "signin";
+    EUScreenXL.SignInPage.prototype.events = {
+        'click #searchbutton': function(event) {
+            if(this.searchButton.hasClass("active")) {
+                this.searchButton.removeClass("active"); // toggle style
+                this.$navbarElement.removeClass('searchOpened');
+                this.menuButton.show();
+            } else {
+                this.searchButton.addClass("active"); // toggle style
+                this.$navbarElement.addClass('searchOpened');
+                this.$formElement.find('input[type="text"]').focus();
+                this.menuButton.hide();
+            }
+        }
+    }
+
+    new EUScreenXL.SignInPage();
+});
